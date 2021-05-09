e662cd2fc4308783a8a9aec206b3fd366503f792
AminRafaey <aminrafaey543@gmail.com>
2021-05-10 00:25:33 +0500

    connection modal issue fix

--- WsRoute/index.js
+++ WsRoute/index.js
@@ -11,19 +11,25 @@ module.exports = function (io) {
     socket.personal = {};
     socket.personal.isChatsSent = false;
     socket.personal.isContactsSent = false;
-    socket.on('get-qr', () => {
+    socket.on('get-qr', (currentConnRef) => {
       async function connectToWhatsApp() {
         const conn = new WAConnection();
+
         conn.connectOptions.maxRetries = 1;
 
         conn.on('qr', (qr) => {
-          io.to(socket.id).emit('get-qr', qr);
+          io.to(socket.id).emit('get-qr', {
+            qr: qr,
+            currentConnRef: currentConnRef,
+          });
         });
 
         conn.on('chats-received', () => {
           if (!socket.personal.isChatsSent) {
             io.to(socket.id).emit('chats-received', conn.chats);
             socket.personal.isChatsSent = true;
+          } else {
+            io.to(socket.id).emit('chats-received', {});
           }
         });
 
@@ -53,28 +59,32 @@ module.exports = function (io) {
           io.to(socket.id).emit('get-contact-messages', { messages, jid: jid });
         });
 
-        socket.on('send-text-message', async ({ mobileNumbers, message }) => {
-          for (number of mobileNumbers) {
-            const lead = await Lead.findOne({ phone: `+${number}` });
-            let convertedMsg = message;
-            lead &&
-              keywords.map((k) => {
-                convertedMsg = convertedMsg.replace(
-                  new RegExp(`__${k.title}__`, 'g'),
-                  lead[k.value]
-                );
-              });
-            conn.sendMessage(
-              `${number}@s.whatsapp.net`,
-              convertedMsg,
-              MessageType.text
-            );
+        socket.on(
+          'send-text-message',
+          async ({ mobileNumbers, message }, arg2, cb) => {
+            for (number of mobileNumbers) {
+              const lead = await Lead.findOne({ phone: `+${number}` });
+              let convertedMsg = message;
+              lead &&
+                keywords.map((k) => {
+                  convertedMsg = convertedMsg.replace(
+                    new RegExp(`__${k.title}__`, 'g'),
+                    lead[k.value]
+                  );
+                });
+              conn.sendMessage(
+                `${number}@s.whatsapp.net`,
+                convertedMsg,
+                MessageType.text
+              );
+            }
+            cb();
           }
-        });
+        );
 
         socket.on(
           'send-image',
-          async ({ mobileNumbers, message, mediaPath }) => {
+          async ({ mobileNumbers, message, mediaPath }, arg2, cb) => {
             try {
               const buffer = fs.readFileSync(
                 path.join(__dirname, '../public/media', mediaPath)
@@ -98,7 +108,7 @@ module.exports = function (io) {
                   }
                 );
               }
-
+              cb();
               deleteFile(path.join(__dirname, '../public/media', mediaPath));
             } catch (err) {
               deleteFile(path.join(__dirname, '../public/media', mediaPath));
@@ -108,7 +118,7 @@ module.exports = function (io) {
 
         socket.on(
           'send-video',
-          async ({ mobileNumbers, message, mediaPath }) => {
+          async ({ mobileNumbers, message, mediaPath }, arg2, cb) => {
             try {
               const buffer = fs.readFileSync(
                 path.join(__dirname, '../public/media', mediaPath)
@@ -132,7 +142,7 @@ module.exports = function (io) {
                   }
                 );
               }
-
+              cb();
               deleteFile(path.join(__dirname, '../public/media', mediaPath));
             } catch (err) {
               deleteFile(path.join(__dirname, '../public/media', mediaPath));
@@ -140,49 +150,50 @@ module.exports = function (io) {
           }
         );
 
-        socket.on('send-pdf', async ({ mobileNumbers, message, mediaPath }) => {
-          try {
-            const buffer = fs.readFileSync(
-              path.join(__dirname, '../public/media', mediaPath)
-            );
-            for (number of JSON.parse(mobileNumbers)) {
-              const lead = await Lead.findOne({ phone: `+${number}` });
-              let convertedMsg = message;
-              lead &&
-                keywords.map((k) => {
-                  convertedMsg = convertedMsg.replace(
-                    new RegExp(`__${k.title}__`, 'g'),
-                    lead[k.value]
-                  );
-                });
-              conn.sendMessage(
-                `${number}@s.whatsapp.net`,
-                buffer,
-                MessageType.document,
-                { mimetype: Mimetype.pdf }
+        socket.on(
+          'send-pdf',
+          async ({ mobileNumbers, message, mediaPath }, arg2, cb) => {
+            try {
+              const buffer = fs.readFileSync(
+                path.join(__dirname, '../public/media', mediaPath)
               );
-              message &&
+              for (number of JSON.parse(mobileNumbers)) {
+                const lead = await Lead.findOne({ phone: `+${number}` });
+                let convertedMsg = message;
+                lead &&
+                  keywords.map((k) => {
+                    convertedMsg = convertedMsg.replace(
+                      new RegExp(`__${k.title}__`, 'g'),
+                      lead[k.value]
+                    );
+                  });
                 conn.sendMessage(
                   `${number}@s.whatsapp.net`,
-                  convertedMsg,
-                  MessageType.text
+                  buffer,
+                  MessageType.document,
+                  { mimetype: Mimetype.pdf }
                 );
+                message &&
+                  conn.sendMessage(
+                    `${number}@s.whatsapp.net`,
+                    convertedMsg,
+                    MessageType.text
+                  );
+              }
+              cb();
+              deleteFile(path.join(__dirname, '../public/media', mediaPath));
+            } catch (err) {
+              console.log(err);
+              deleteFile(path.join(__dirname, '../public/media', mediaPath));
             }
-
-            deleteFile(path.join(__dirname, '../public/media', mediaPath));
-          } catch (err) {
-            console.log(err);
-            deleteFile(path.join(__dirname, '../public/media', mediaPath));
           }
-        });
-        conn.on('close', ({ reason, isReconnecting }) =>
-          console.log(
-            'oh no got disconnected: ' +
-              reason +
-              ', reconnecting: ' +
-              isReconnecting
-          )
         );
+        conn.on('close', ({ reason, isReconnecting }) => {
+          io.to(socket.id).emit('disconnected', {
+            message: 'Disconnected from WhatsApp: ' + reason,
+            currentConnRef: currentConnRef,
+          });
+        });
       }
       connectToWhatsApp().catch((err) => {
         io.to(socket.id).emit('no-qr', null);

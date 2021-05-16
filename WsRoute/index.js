394a06603b9808571a60ed0e1cdfda9f75f5b2f0
AminRafaey <aminrafaey543@gmail.com>
2021-05-17 00:57:38 +0500

    handle incoming message on runtinr

--- WsRoute/index.js
+++ WsRoute/index.js
@@ -1,4 +1,9 @@
-const { WAConnection, MessageType, Mimetype } = require('@adiwajshing/baileys');
+const {
+  WAConnection,
+  MessageType,
+  Mimetype,
+  WA_MESSAGE_STUB_TYPES,
+} = require('@adiwajshing/baileys');
 const { keywords } = require('../Static/Keyword');
 const fs = require('fs');
 const path = require('path');
@@ -53,6 +58,55 @@ module.exports = function (io) {
         });
 
         await conn.connect();
+
+        conn.on('chat-new', (chat) => {
+          socket.emit('chat-new', chat);
+        });
+
+        conn.on('chat-update', async (chat) => {
+          if (chat.presences) {
+            // receive presence updates -- composing, available, etc.
+            Object.values(chat.presences).forEach((presence) =>
+              console.log(
+                `${presence.name}'s presence is ${presence.lastKnownPresence} in ${chat.jid}`
+              )
+            );
+          }
+          if (chat.imgUrl) {
+            console.log('imgUrl of chat changed ', chat.imgUrl);
+            return;
+          }
+          // only do something when a new message is received
+          if (!chat.hasNewMessage) {
+            if (chat.messages) {
+              console.log('updated message: ', chat.messages.first);
+            }
+            return;
+          }
+
+          const m = chat.messages.all()[0]; // pull the new message from the update
+          const messageStubType =
+            WA_MESSAGE_STUB_TYPES[m.messageStubType] || 'MESSAGE';
+          console.log('got notification of type: ' + messageStubType);
+
+          const messageContent = m.message;
+          // if it is not a regular text or media message
+          if (!messageContent) return;
+
+          if (m.key.fromMe) {
+            console.log('relayed my own message');
+            return;
+          }
+
+          let sender = m.key.remoteJid;
+          if (m.key.participant) {
+            // participant exists if the message is in a group
+            sender += ' (' + m.key.participant + ')';
+          } else {
+            m.key.remoteJid.split('@')[1] === 's.whatsapp.net' &&
+              socket.emit('new-message', m);
+          }
+        });
 
         socket.on('get-contact-messages', async (jid) => {
           const messages = await conn.loadMessages(jid, 100);
@@ -198,7 +252,6 @@ module.exports = function (io) {
           }
         );
         conn.on('close', ({ reason, isReconnecting }) => {
-          console.log('here', reason);
           io.to(socket.id).emit('disconnected', {
             message: 'Disconnected from WhatsApp: ' + reason,
             currentConnRef: currentConnRef,

daed7370129a291af147f3afa9167cd5e53371e5
AminRafaey <aminrafaey543@gmail.com>
2021-06-02 00:37:34 +0500

    CRUD for employee routes

--- routes/Employee.js
+++ routes/Employee.js
@@ -5,6 +5,11 @@ const phone = require('phone');
 const { Employee, validateEmployee } = require('../models/Employee');
 const { Admin } = require('../models/Admin');
 const { User } = require('../models/User');
+const {
+  validateFilter,
+  validateEmployeeUpdate,
+} = require('./RouteHelpers/Employee');
+const { validateObjectId } = require('./RouteHelpers/Common');
 
 router.post('/', async (req, res) => {
   try {
@@ -100,6 +105,212 @@ router.post('/', async (req, res) => {
           name: 'successful',
         },
       });
+  } catch (error) {
+    res.status(500).send({
+      field: { message: 'Unexpected error occured', name: 'unexpected' },
+    });
+  }
+});
+
+router.get('/all', async (req, res) => {
+  try {
+    res.status(200).send({
+      field: {
+        name: 'successful',
+        message: 'Successfully Fetched',
+        data: await Employee.find(),
+      },
+    });
+  } catch (error) {
+    res.status(500).send({
+      field: { message: 'Unexpected error occured', name: 'unexpected' },
+    });
+  }
+});
+
+router.get('/', async (req, res) => {
+  try {
+    const { _id } = req.query;
+    const { error } = validateObjectId(req.query);
+    if (error) {
+      return res.status(400).send({
+        field: {
+          message: error.details[0].message,
+          name: error.details[0].path[0],
+        },
+      });
+    }
+    res.status(200).send({
+      field: {
+        name: 'successful',
+        message: 'Successfully Fetched',
+        data: await Employee.findById(_id),
+      },
+    });
+  } catch (error) {
+    res.status(500).send({
+      field: { message: 'Unexpected error occured', name: 'unexpected' },
+    });
+  }
+});
+
+router.get('/filter', async (req, res) => {
+  try {
+    req.query = JSON.stringify(req.query);
+    req.query = JSON.parse(req.query);
+    Object.keys(req.query).map(
+      (f) => (req.query[f] = req.query[f].map((obj) => JSON.parse(obj)))
+    );
+    Object.keys(req.query).map((f) => (req.query[f] = eval(req.query[f])));
+
+    const {
+      firstNames = [{}],
+      lastNames = [{}],
+      designations = [{}],
+      mobileNumbers = [{}],
+      statuses = [{}],
+    } = req.query;
+
+    const { error } = validateFilter(req.query);
+    if (error) {
+      return res.status(400).send({
+        field: {
+          message: error.details[0].message,
+          name: error.details[0].path[0],
+        },
+      });
+    }
+
+    const query = eval({
+      $and: [
+        { $or: [...firstNames] },
+        { $or: [...lastNames] },
+        { $or: [...designations] },
+        { $or: [...mobileNumbers] },
+        { $or: [...statuses] },
+      ],
+    });
+
+    const employees = await Employee.find(query);
+
+    res.status(200).send({
+      field: {
+        name: 'successful',
+        message: 'Successfully Fetched',
+        data: employees,
+      },
+    });
+  } catch (error) {
+    res.status(500).send({
+      field: { message: 'Unexpected error occured', name: 'unexpected' },
+    });
+  }
+});
+
+router.put('/', async (req, res) => {
+  try {
+    const { _id, createdAt, __v, updatedAt, ...data } = req.body;
+
+    const { error } = validateEmployeeUpdate(data);
+    if (error)
+      return res.status(400).send({
+        field: {
+          message: error.details[0].message,
+          name: error.details[0].path[0],
+        },
+      });
+    const { error: error2 } = validateObjectId({ _id: _id });
+    if (error2) {
+      return res.status(400).send({
+        field: {
+          message: error2.details[0].message,
+          name: error2.details[0].path[0],
+        },
+      });
+    }
+
+    if (!(await Employee.findById(_id))) {
+      return res.status(400).send({
+        field: {
+          name: 'Employee Id',
+          message: 'No Employee with this Id exist',
+        },
+      });
+    }
+
+    const { mobileNumber } = data;
+
+    if (mobileNumber && phone(mobileNumber).length === 0) {
+      return res.status(400).send({
+        field: {
+          name: 'phone',
+          message: 'Phone number is not valid',
+        },
+      });
+    }
+
+    if (
+      mobileNumber &&
+      (await Employee.findOne().and([
+        {
+          mobileNumber: mobileNumber,
+        },
+        { _id: { $ne: _id } },
+      ]))
+    ) {
+      return res.status(400).send({
+        field: {
+          name: 'phone',
+          message:
+            'Sorry, duplicate Employee found with the same phone number.',
+        },
+      });
+    }
+
+    await Employee.updateOne({ _id: _id }, { ...data, updatedAt: Date() });
+
+    res.status(200).send({
+      field: {
+        name: 'successful',
+        message: 'Successfully updated',
+        data: await Employee.findById(_id),
+      },
+    });
+  } catch (error) {
+    res.status(500).send({
+      field: { message: 'Unexpected error occured', name: 'unexpected' },
+    });
+  }
+});
+
+router.delete('/', async (req, res) => {
+  try {
+    const employeeId = req.query.employeeId;
+
+    const { error } = validateObjectId({ _id: employeeId });
+    if (error)
+      return res.status(400).send({
+        field: {
+          message: error.details[0].message,
+          name: error.details[0].path[0],
+        },
+      });
+
+    await Employee.deleteOne({
+      _id: employeeId,
+    });
+
+    await User.deleteOne({
+      employeeId: employeeId,
+    });
+
+    res.send({
+      field: {
+        name: 'successful',
+        message: 'Successfully Deleted',
+        data: {},
+      },
+    });
   } catch (error) {
     res.status(500).send({
       field: { message: 'Unexpected error occured', name: 'unexpected' },
---
+++ routes/RouteHelpers/Employee.js
@@ -0,0 +1,68 @@
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
+
+const validateFilter = (filters) => {
+  const schema = Joi.object({
+    firstNames: Joi.array()
+      .items(
+        Joi.object()
+          .keys({
+            firstName: Joi.optional(),
+          })
+          .required()
+      )
+      .optional(),
+    lastNames: Joi.array()
+      .items(
+        Joi.object()
+          .keys({
+            lastName: Joi.optional(),
+          })
+          .required()
+      )
+      .optional(),
+    designations: Joi.array()
+      .items(
+        Joi.object()
+          .keys({
+            designation: Joi.optional(),
+          })
+          .required()
+      )
+      .optional(),
+    mobileNumbers: Joi.array()
+      .items(
+        Joi.object()
+          .keys({
+            mobileNumber: Joi.optional(),
+          })
+          .required()
+      )
+      .optional(),
+    statuses: Joi.array()
+      .items(
+        Joi.object()
+          .keys({
+            status: Joi.optional(),
+          })
+          .required()
+      )
+      .optional(),
+  });
+  return schema.validate(filters);
+};
+
+const validateEmployeeUpdate = (data) => {
+  const schema = Joi.object({
+    firstName: Joi.string().required(),
+    lastName: Joi.string().allow('').optional(),
+    designation: Joi.string().required(),
+    status: Joi.string().valid('Active', 'Blocked').required(),
+    mobileNumber: Joi.string().required(),
+    joiningDate: Joi.date().required(),
+  });
+  return schema.validate(data);
+};
+
+exports.validateFilter = validateFilter;
+exports.validateEmployeeUpdate = validateEmployeeUpdate;
